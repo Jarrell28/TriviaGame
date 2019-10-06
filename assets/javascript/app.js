e51e5e66662afc16995678cbfb17913be9139b70
Jarrell Houston <jhouston2882@yahoo.com>
2019-10-06 17:47:39 -0500

    Added basic html markup and psuedo code for game logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//Create HTML Selecters
+
+//Create Trivia Game Object
+//array of question objects
+//right answers
+//wrong answers
+//unanswered
+//playing boolean
+//number of questions remaining
+
+//method to check right/wrong
+//when select answer display correct answer with image
+
+//method to reset game
