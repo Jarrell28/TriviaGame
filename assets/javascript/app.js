64fa863c9a2be7fe2683165373f38d27aa444b2c
Jarrell Houston <jhouston2882@yahoo.com>
2019-10-06 18:37:38 -0500

    Coded layout of game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,53 @@
-//Create HTML Selecters
-
 //Create Trivia Game Object
-//array of question objects
-//right answers
-//wrong answers
-//unanswered
-//playing boolean
-//number of questions remaining
+var TriviaObj = {
+    questions: [],
+    playing: false,
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    remaining: 0,
 
-//method to check right/wrong
-//when select answer display correct answer with image
+    startGame: function () {
 
-//method to reset game
+    },
+
+    reset: function () {
+
+    },
+
+    checkAnswer: function () {
+        //when select answer display correct answer with image
+
+    },
+
+    gameResults: function () {
+
+    }
+
+
+}
+
+
+
+
+//Event Listeners
+$(document).ready(function () {
+    //start game
+})
+
+$("#start").on("click", function () {
+    //start game
+});
+
+$("#play-again").on("click", function () {
+    //start game
+})
+
+$("#question").on("click", ".choice", function () {
+    //check answer
+})
+
+
+
+
+
