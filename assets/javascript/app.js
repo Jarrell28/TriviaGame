d6d2c260079dd1bace55dab2c707cc825d527faa
Jarrell Houston <jhouston2882@yahoo.com>
2019-10-09 21:08:54 -0500

    Inserted code comments explaining each part of the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,9 +60,12 @@ var TriviaObj = {
     timer: "",
 
     startGame: function () {
+        //Initializes Game from start button
         $("#start").hide();
-        this.gameQuestions = Object.assign(this.questions);
 
+        //Makes a copy of questions array
+        this.gameQuestions = Object.assign(this.questions);
+        //Gets first question
         this.getQuestion();
         $("#question").show();
         $("#time-container").show();
@@ -80,51 +83,72 @@ var TriviaObj = {
     checkAnswer: function (answer) {
         //when select answer display correct answer with image
 
+        //When they answered correctly
         if (answer === this.correctAnswer) {
             alert("Correct");
             this.correct++;
             console.log(this.correct);
-        } else {
+        }
+
+        //When they answered incorrectly
+        else {
             alert("Incorrect");
             this.incorrect++;
             console.log(this.incorrect);
         }
 
+        //Goes to next question
         this.getQuestion();
+        //Clears timer interval
         clearInterval(this.timer);
 
 
     },
 
+    //All questions answered, shows scores
     gameResults: function () {
         clearInterval(this.timer);
         alert("Game Over");
     },
 
+    //Gets a random question
     getQuestion: function () {
-
+        //Sets remaining questions variable to length of gameQuestions array
         this.remaining = this.gameQuestions.length;
 
+        //If there are still questions left
         if (this.remaining > 0) {
+            //Resets seconds left for question
             this.secondsLeft = 30;
+            //Clears any previous timer interval
             clearInterval(this.timer);
+            //Sets the timer interval
             this.timer = setInterval(function () {
+                //Updates time html
                 $("#time").text(TriviaObj.secondsLeft);
 
+                //Checks if player has run out of time
                 if (TriviaObj.secondsLeft === 0) {
                     TriviaObj.unanswered++;
                     console.log(TriviaObj.unanswered);
+                    //Goes to next question
                     TriviaObj.getQuestion();
+                } else {
+                    //Decrements seconds left each second
+                    TriviaObj.secondsLeft--;
                 }
-                TriviaObj.secondsLeft--;
             }, 1000);
 
+            //Gets random number for question index
             var random = Math.floor(Math.random() * this.gameQuestions.length);
 
+            //sets variable as question object
             var currentQuestion = this.gameQuestions[random];
 
+            //sets correct answer variable to the correct answer from the currentQuestion object
             this.correctAnswer = currentQuestion.correct;
 
+            //Sets html to display question and choices
             $("#question").empty();
             $("#question").append("<h4>" + currentQuestion.question + "</h4>");
             $("#question").append("<p class='choice' data-answer='" + currentQuestion.answer1 + "'>" + currentQuestion.answer1 + "</p>");
@@ -132,8 +156,10 @@ var TriviaObj = {
             $("#question").append("<p class='choice' data-answer='" + currentQuestion.answer3 + "'>" + currentQuestion.answer3 + "</p>");
             $("#question").append("<p class='choice' data-answer='" + currentQuestion.answer4 + "'>" + currentQuestion.answer4 + "</p>");
 
+            //Removes current question from gameQuestions array
             this.gameQuestions.splice(random, 1);
         } else {
+            //if no more questions then shows game results
             this.gameResults();
         }
     }
