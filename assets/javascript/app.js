47e92900fa9166d763645de094e031e341f3d2cd
Jarrell Houston <jhouston2882@yahoo.com>
2019-10-12 12:31:16 -0500

    Logic completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,7 +56,7 @@ var TriviaObj = {
     unanswered: 0,
     remaining: 0,
     correctAnswer: "",
-    secondsLeft: 30,
+    secondsLeft: 8,
     timer: "",
 
     startGame: function () {
@@ -85,20 +85,24 @@ var TriviaObj = {
 
         //When they answered correctly
         if (answer === this.correctAnswer) {
-            alert("Correct");
             this.correct++;
-            console.log(this.correct);
+            $("#question").empty();
+            $("#question-result").html("<p class='text-center'>Correct!</p>");
         }
 
         //When they answered incorrectly
         else {
-            alert("Incorrect");
             this.incorrect++;
-            console.log(this.incorrect);
+            $("#question").empty();
+            $("#question-result").html("<p class='text-center'>Wrong Answer!</p>");
+
         }
 
         //Goes to next question
-        this.getQuestion();
+        setTimeout(function () {
+            TriviaObj.getQuestion();
+        }, 5000)
+
         //Clears timer interval
         clearInterval(this.timer);
 
@@ -108,7 +112,15 @@ var TriviaObj = {
     //All questions answered, shows scores
     gameResults: function () {
         clearInterval(this.timer);
-        alert("Game Over");
+        $("#question").hide();
+        $("#time-container").hide();
+        $("#correct").text(this.correct);
+        $("#incorrect").text(this.incorrect);
+        $("#unanswered").text(this.unanswered);
+        //Reset Question Result Html
+        $("#question-result").empty();
+        $("#results").show();
+        $("#play-again").show();
     },
 
     //Gets a random question
@@ -119,24 +131,25 @@ var TriviaObj = {
         //If there are still questions left
         if (this.remaining > 0) {
             //Resets seconds left for question
-            this.secondsLeft = 30;
+            this.secondsLeft = 8;
+            $("#time").text(this.secondsLeft);
             //Clears any previous timer interval
             clearInterval(this.timer);
             //Sets the timer interval
             this.timer = setInterval(function () {
+
+                TriviaObj.secondsLeft--;
                 //Updates time html
                 $("#time").text(TriviaObj.secondsLeft);
 
                 //Checks if player has run out of time
                 if (TriviaObj.secondsLeft === 0) {
                     TriviaObj.unanswered++;
-                    console.log(TriviaObj.unanswered);
+
                     //Goes to next question
-                    TriviaObj.getQuestion();
-                } else {
-                    //Decrements seconds left each second
-                    TriviaObj.secondsLeft--;
+                    TriviaObj.checkAnswer();
                 }
+
             }, 1000);
 
             //Gets random number for question index
@@ -147,6 +160,9 @@ var TriviaObj = {
 
             //sets correct answer variable to the correct answer from the currentQuestion object
             this.correctAnswer = currentQuestion.correct;
+
+            //Reset Question Result Html
+            $("#question-result").empty();
 
             //Sets html to display question and choices
             $("#question").empty();
@@ -179,8 +195,4 @@ $("#question").on("click", ".choice", function () {
     //check answer
     TriviaObj.checkAnswer($(this).attr("data-answer"));
 })
-
-
-
-
 
